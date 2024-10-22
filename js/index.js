5de035e8cdb2c578d63a10e525039d86ac4b7892
FlorBee <buhleflor@gmail.com>
2024-10-22 22:19:02 +0200

    Added an all cities link

--- js/index.js
+++ js/index.js
@@ -47,7 +47,8 @@ function updateCity(event) {
             <div class="time">${cityTime.format(
               "h:mm:mm"
             )}<small> ${cityTime.format("A")}</small></div>
-          </div>`;
+          </div>
+          <a href="index.html">All cities </a>`;
 }
 
 updateTime();
